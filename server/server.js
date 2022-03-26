5d872369c62288f5a4842d57fe5d11053000f8f8
Sophie Engobor <sophietengobor@gmail.com>
2022-03-26 04:52:55 -0500

    almost there

--- server/server.js
+++ server/server.js
@@ -1,21 +1,22 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 let app = express();
 
 app.use((req, res, next) => {
-    console.log(req.originalUrl);
+    console.log(req.url);
     next();
 });
 
-app.get("/", (req, res) => {
-    res.send('Hello from the web server side...');
-});
+// app.get("/", (req, res) => {
+//     res.send('Hello from the web server side...');
+// });
 
 app.use(express.static(path.join(__dirname, "../public")));
 
 
 
-app.listen(3000);
+app.listen(3000, () => console.log('hello from the web server...'));
 
 
 // app.use("/order/:name", (req, res) => {
